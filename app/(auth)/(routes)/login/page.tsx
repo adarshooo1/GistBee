714a3797a5b9157cdd1f811775ca3eb4c075158f
Adarsh Singh <adarshsinghak001@gmail.com>
2023-10-26 19:19:40 +0530

    GistBee Project Setup Completed now we will move to the landing Page

---
+++ app/(auth)/(routes)/login/page.tsx
@@ -0,0 +1,9 @@
+const LoginPage = () => {
+  return (
+    <>
+      <div>Welcome to login page</div>
+    </>
+  );
+};
+
+export default LoginPage;
---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,9 @@
+const RegisterPage = () => {
+  return (
+    <>
+      <div>Welcome to register page</div>
+    </>
+  );
+};
+
+export default RegisterPage;
